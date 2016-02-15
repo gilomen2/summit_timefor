b494072583e0a02c723aeec24c5573a46b1a4b21
Beth Gilomen <gilomen2@gmail.com>
2016-02-15 14:06:22 -0600

    removed unnecessary comments

--- src/main.lua
+++ src/main.lua
@@ -4,9 +4,6 @@ local json = require 'json'
 local inspect = require 'inspect'
 local application = require("summit.application")
 local myData = application.get_data()
--- local myData = "{\"message\": \"This is my message from Postman.\", \"name\": \"Beth\"}"
--- local decodedString = json:decode(myData)
--- print(inspect(decodedString))
 
 local contactName = myData.contactName
 local senderName = myData.senderName
