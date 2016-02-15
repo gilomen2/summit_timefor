99ed4b26cc6d0efa5a3870e566284875fcd19ba2
Beth Gilomen <beth.gilomen@corvisa.com>
2016-02-15 19:27:55 +0000

    first commit

--- src/main.lua
+++ src/main.lua
@@ -1,9 +1,16 @@
 -- This comment enforces unit-test coverage for this file:
 -- coverage: 0
+local json = require 'json'
+local inspect = require 'inspect'
+local application = require("summit.application")
+local myData = application.get_data()
+-- local myData = '{"name":"Beth","message":"This is the message."}'
+local decodedString = json:decode(myData)
+
+
+local name = decodedString.name
+local message = decodedString.message
 
 channel.answer()
-channel.say("This is an example application. Enter any number followed by the pound sign.")
-local digits = channel.gather()
-channel.say(digits)
+channel.say('This message is for'.. name .. '. ' .. message)
 channel.hangup()
-
